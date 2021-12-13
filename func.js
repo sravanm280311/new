8ae85336e9fda2afc1688bb3c549c64762caa303
sravanm280311 <sravanmclt@gmail.com>
2021-12-13 06:44:02 +0530

    second commit

---
+++ func.js
@@ -0,0 +1,20 @@
+// function add(a, b) {
+//   return a + b;
+// }
+
+// // take input from the user
+// let number1 = 8;
+// let number2 = 7;
+
+// // calling function
+// let result = add(number1, number2);
+
+// // display the result
+// console.log("The sum is " + result);
+
+// Square using function
+
+let sq = function (num) {
+  return num * num;
+};
+console.log(sq(4));
